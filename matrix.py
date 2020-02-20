f5367db4ef9c8c6501e603b7a24427074df0d172
imran khan <59873548+imranalpha@users.noreply.github.com>
2020-02-20 21:28:42 +0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -10,6 +10,7 @@ def Traversing(mat):
         return score
     for x in mat:
         for y in x:
+            print(col,row)
             if(col+1<len(mat)):
                 if(row+1<len(x)):
                     if(mat[col+1][row]==1):
@@ -42,19 +43,15 @@ def Traversing(mat):
                     else:
                         break
             lastvalue=mat[col][row]
-            
-            if(col&row==2):
-                end_reched=True
-            print(col,row)
             if(mat[col][row]==1):
                 mat[col][row]=0
                 score+=1
-            #print("score ",score)
-
-            if(col>len(mat)-2)and(row>len(mat)-2):
-                break           
+            if(col&row==2):
+                end_reched=True
+                break
     for x in mat:
         for y in x:
+            print(col,row)
             if(col>0):
                 if(row>0):
                     if(mat[col-1][row]==1):
@@ -81,7 +78,6 @@ def Traversing(mat):
                         row-=1
                     else:
                         break
-            print(col,row)
             lastvalue=mat[col][row]
             if(mat[col][row]==1):
                 mat[col][row]=0
